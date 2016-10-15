2eb1a16222b5003a4ce21db207cc3d8d2eb02e53
Juan Reyes <juanignacioreyesgfeller@gmail.com>
2016-10-15 16:25:42 -0300

    Create menu_proyecto.sh

---
+++ menu_proyecto.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+clear
+while [ "$var" !=5 ]
+do
+        echo "
+                1) Ingresar pago
+                2) Emitir estado de cuenta
+                3) Situación de cada propietario
+                4) Atrasos de gastos comunes
+                5) Salir
+                Ingrese opción: "
+        read var
+case $var in
+1) ./ingreso.sh;;
+2) ./estado.sh;;
+3) ./situacion.sh;;
+4) ./atrasos.sh;;
+5) echo salir...
+*) echo Opcion incorrecta
+esac
+done
