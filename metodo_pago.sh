c527bc3499cd737a962e86f403013cb13b73a28a
Juan Reyes <juanignacioreyesgfeller@gmail.com>
2016-10-17 11:24:36 -0300

    Create metodo_pago.sh

---
+++ metodo_pago.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+while [ "$var" != 3 ]
+do
+        echo "
+                1) Al contado
+                2) Verificar pago a través de la banca electrónica
+                3) Volver
+                Ingrese opción: "
+        read var
+        case $var in
+                1) ./ingreso.sh;;
+                2) ./verificar.sh;;
+                3) echo Regresando... ; sleep 1;;
+                *) echo Opcion incorrecta ; sleep 1
+        esac
+done                                                                            
