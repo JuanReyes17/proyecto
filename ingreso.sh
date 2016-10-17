f8ee62677a8bd9dd5df6b119f20365e1cb6bd4d0
Juan Reyes <juanignacioreyesgfeller@gmail.com>
2016-10-17 11:26:07 -0300

    Update ingreso.sh

--- ingreso.sh
+++ ingreso.sh
@@ -1,13 +1,17 @@
+
 #!/bin/bash
 clear
-f=`date +%d`
-if [ $f -le 10 ]
+f=$(date +%d/%m/%Y)
+echo "Hoy es $f"
+dia=`date +%d`
+if [ $dia -le 10  ]
 then
         read -p "Ingrese número de apartamento: " id_apartamento
         if grep -wq $id_apartamento propietarios.txt 2> /dev/null
         then
                 read -p "El pago corresponde al mes: " corresponde_a_mes
-                monto="2100"
+                monto="1995"
+                echo -n "El monto es: $monto"
                 echo $id_apartamento:$corresponde_a_mes:$monto >> pagos.txt
         else
                 echo -n "Número de apartamento incorrecto, para ingresar uno nuevo presione cualquier tecla, para salir s: "
@@ -18,7 +22,20 @@ then
                 fi
         fi
 else
-         echo -n "Solo se pueden realizar pagos del 1º al 10 de cada mes" 
-sleep 2
-clear
+        read -p "Ingrese número de apartamento: " id_apartamento
+        if grep -wq $id_apartamento propietarios.txt 2> /dev/null
+        then
+                read -p "El pago corresponde al mes: " corresponde_a_mes
+                monto1="2100"
+                echo -n "El monto es: $monto1"
+                echo $id_apartamento:$corresponde_a_mes:$monto >> pagos.txt
+        else
+                echo -n "Número de apartamento incorrecto, para ingresar uno nuevo presione cualquier tecla, para salir s: "
+                read op
+                if [ $op = s ]
+                then
+                        echo saliendo...
+                fi
+        fi
 fi
+sleep 1
