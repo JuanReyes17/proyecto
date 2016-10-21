d48607f7ca8bdf216e51cae9eeea545d6500f3a1
Juan Reyes <juanignacioreyesgfeller@gmail.com>
2016-10-21 16:08:25 -0300

    Update menu_proyecto.sh

--- menu_proyecto.sh
+++ menu_proyecto.sh
@@ -1,21 +1,19 @@
 #!/bin/bash 
 clear
-while [ "$var" != 5 ]
+while [ "$var" != 4 ]
 do
         echo "  ¿Qué desea relizar?
                 1) Ingresar pago
-                2) Emitir estado de cuenta
-                3) Situación de cada propietario
-                4) Atrasos de gastos comunes
-                5) Salir
+                2) Situación de cada propietario
+                3) Atrasos de gastos comunes
+                4) Salir
                 Ingrese opción: "
         read var
 case $var in
 1) ./metodo_pago.sh;;
-2) ./estado.sh;;
-3) ./situacion.sh;;
-4) ./atrasos.sh;;
-5) echo -e '\e[0;36m Saliendo...\e[0m' ; sleep 1;;
+2) ./situacion.sh;;
+3) ./atrasos.sh;;
+4) echo -e '\e[0;36m Saliendo...\e[0m' ; sleep 1;;
 *) echo -e '\e[0;31m Opción incorrecta\e[0m' ; sleep 1
 esac
 done
