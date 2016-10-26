bfef0c4994a94a678e72e6e9119e7c664f8f91c5
Juan Reyes <juanignacioreyesgfeller@gmail.com>
2016-10-26 11:51:33 -0300

    Update metodo_pago.sh

--- metodo_pago.sh
+++ metodo_pago.sh
@@ -2,16 +2,16 @@
 clear
 while [ "$var" != 3 ]
 do
-        echo "  ¿De qué forma desea abonar?
-                1) Al contado
-                2) Verificar pago a través de la banca electrónica
-                3) Volver
-                Ingrese opción: "
-        read var
-        case $var in
-                1) ./ingreso.sh;;
-                2) ./verificar.sh;;
-                3) echo -e '\e[0;32m Regresando...\e[0m'; sleep 1;;
-                *) echo -e '\e[0;31m Opción incorrecta\e[0m'; sleep 1
-        esac
-done                                                                            
+	echo "
+		1) Al contado
+		2) Verificar pago a través de la banca electrónica
+		3) Volver
+		Ingrese opción: "
+	read var
+	case $var in
+		1) ./pagar.sh;;
+		2) ./verificar.sh;;
+		3) echo -e '\e[0;32m Regresando...\e[0m'; sleep 1 ; clear;;
+		*) echo -e '\e[0;31m Opción incorrecta\e[0m'; sleep 1; clear 
+	esac
+done
