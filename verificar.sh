6ae2bbe9c1c1a82c9cf5d11387de01dcfce84996
Juan Reyes <juanignacioreyesgfeller@gmail.com>
2016-10-26 11:50:17 -0300

    Update verificar.sh

--- verificar.sh
+++ verificar.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 clear
-read -p "Ingrese número de apartamento: " id_apartamento
-if grep -wq $id_apartamento pagos.txt 2> /dev/null
-then
-        echo -n "El pago ah sido realizado con éxito"
-        sleep 1
-        exit
-else
-        echo -n "No se ah efectuado ningun pago, presione cualquier tecla verificar otro pago, presione s para volver: "
-        read op
-        if [ $op = s ]
-        then
-                echo -e '\e[0;36m Saliendo...\e[0m'
-        fi
-        sleep 1
-        exit
-fi
+read -p "ingrese apartamento: " PAGO_APT;
+read -p "ingrese mes a verificar: " PAGO_MES;
+PAGO_MES_POS=$(($PAGO_MES + 1)); # por la columna con el id de apartamento
+while read APT_LINE;
+do 
+	APT=$(echo $APT_LINE |awk '{print $1}'); # extraigo # de apto
+	if [[ "A${PAGO_APT}" == "${APT}" ]];
+	then
+       		 # si el apto actual en el while es el deseado...      
+		APT_LINE_MES=$(echo $APT_LINE |cut -d " " -f $PAGO_MES_POS); # obtengo dato actual para el mes que se desea pagar
+		if [[ "${APT_LINE_MES}" == "Y" ]];
+       		then
+	        	# si ya pago...
+			echo -n "El pago del mes ${PAGO_MES} para el apartamento ${PAGO_APT} fué realizado con éxito";
+			sleep 1
+			clear
+		else
+			# si no pago
+	         	echo -n "No se ah efectuado el pago del apartamento ${PAGO_APT} para el mes ${PAGO_MES}"
+			sleep 1
+			clear        
+		fi 	
+		
+	fi
+done <<< "$(cat pagos |tail -n +2)";
